869ec483a7e6100ea740e938c7e5a2bccaf357f4
hjl <3229464184@qq.com>
2020-09-11 22:38:47 +0800

    阿里云上传OSS完善

--- course-microservice-oss/src/main/java/com/course/oss/controller/FileController.java
+++ course-microservice-oss/src/main/java/com/course/oss/controller/FileController.java
@@ -2,10 +2,12 @@ package com.course.oss.controller;
 
 import com.course.common.vo.R;
 import com.course.oss.service.FileService;
+import com.course.oss.util.ConstantPropertiesUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -26,8 +28,14 @@ public class FileController {
     @PostMapping("upload")
     public R upload(
             @ApiParam(name = "file", value = "文件", required = true)
-            @RequestParam("file") MultipartFile file
+            @RequestParam("file") MultipartFile file,
+            @ApiParam(name = "host",value = "文件路径",required = false)
+            @RequestParam("host") String host
             ){
+
+        if(!StringUtils.isEmpty(host)){
+            ConstantPropertiesUtil.FILE_HOST = host;
+        }
         String upload = fileService.upload(file);
         return R.ok().message("文件上传成功").data("url",upload);
     }
